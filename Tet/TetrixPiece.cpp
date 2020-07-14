3616f34e4757a49987f01cffb943fad269ff4b6c
Ivan Shukshin <skyligh666@live.com>
2020-07-15 02:24:51 +0300

    ф

--- Tet/TetrixPiece.cpp
+++ Tet/TetrixPiece.cpp
@@ -20,23 +20,16 @@ QColor TetrixPiece::getColor()
     return pieceColor;
 }
 
-void TetrixPiece::setRandomShape()
+void TetrixPiece::setSquareShape()
 {
-    setShape(TetrixShape(9));
+    setShape(TetrixShape(1));
 }
 
 void TetrixPiece::setShape(TetrixShape shape)
 {
-    static constexpr int coordsTable[9][4][2] = {
-        { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
-        { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
-        { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
-        { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
-        { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
-        { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
-        { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
-        { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } },
-        { { 1, 1 },  { 0, 0 },  { 0, 0 },   { 0, 0 } }
+    static constexpr int coordsTable[2][4][2] = {
+        { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } },
+        { { 1, 1 }, { 0, 0 }, { 0, 0 }, { 0, 0 } }
     };
 
     for (int i = 0; i < 4; i++) {
@@ -46,37 +39,5 @@ void TetrixPiece::setShape(TetrixShape shape)
     pieceShape = shape;
 }
 
-int TetrixPiece::minX() const
-{
-    int min = coords[0][0];
-    for (int i = 1; i < 4; ++i)
-        min = qMin(min, coords[i][0]);
-    return min;
-}
-
-int TetrixPiece::maxX() const
-
-{
-    int max = coords[0][0];
-    for (int i = 1; i < 4; ++i)
-        max = qMax(max, coords[i][0]);
-    return max;
-}
-
-int TetrixPiece::minY() const
-{
-    int min = coords[0][1];
-    for (int i = 1; i < 4; ++i)
-        min = qMin(min, coords[i][1]);
-    return min;
-}
-
-int TetrixPiece::maxY() const
-{
-    int max = coords[0][1];
-    for (int i = 1; i < 4; ++i)
-        max = qMax(max, coords[i][1]);
-    return max;
-}
 
 
